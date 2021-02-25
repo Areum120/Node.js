5ac0d65eeee9c4682e5a5bbbc636ef4c00ef39f4
LAPTOP-F3LFLS66\tjfsu <tjfsu120@gmail.com>
2021-02-25 14:31:27 +0900

    v-cloak

---
+++ tutorial14/index.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    el: '#app-13',
+    data: {
+        
+        header: '튜토리얼13 : v-cloak 이아름',
+        message: 'v-cloak은 페이지를 열 떄 순간적으로 {{....}}가 표시되는 것을 방지하는 지시자입니다',
+        message1: 'v-cloak속성이 지정된 요소는 CSS display:none이 적용됩니다',
+        message2: 'vue렌더링이 끝날떄 자동적으로 display:none이 해제됩니다',
+
+
+        message3:'브라우저 새로고침 버튼이나 키보드 F5를 눌러주세요', 
+        message4:'안녕하세요',
+        
+      
+
+
+    },
+    
+    
+
+});
