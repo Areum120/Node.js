f009de5a02c21636399fb7c98a797ed76fdd8800
LAPTOP-F3LFLS66\tjfsu <tjfsu120@gmail.com>
2021-02-25 14:30:57 +0900

    v-pre

---
+++ tutorial13/index.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#app-12',
+    data: {
+        
+        header: '튜토리얼11 : v-\pre 이아름',
+        message: 'v-pre는 텍스트를 그대로 렌더링하는 지시자입니다',
+
+        message1:'v-pre가 아닌 경우', 
+        message2:'v-pre인 경우',
+        
+        value1:'v-pre입니다',
+        value2:'v-pre가 아닙니다'
+
+
+    },
+    
+    
+
+});
